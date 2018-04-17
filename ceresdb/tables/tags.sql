b3ee9821881f6a3b3039b57b894f50dfa9ea5f3b
Gregg Schofield <G.E.schofield@liv.ac.uk>
2018-04-17 19:15:46 +0100

    Changed table to optimise query writing.

--- ceresdb/tables/tags.sql
+++ ceresdb/tables/tags.sql
@@ -1,12 +1,12 @@
--- SQL data definition statements for the recipe_tags table of the Ceres database.
+-- SQL data definition statements for the tags table of the Ceres database.
 -- Created: 19/03/18
 -- Author: Gregg Schofield
 
 -- comment this command out if you are attempting to create this table for the
 -- first time.
---DROP TABLE recipe_tags;
+--DROP TABLE tags;
 
-CREATE TABLE IF NOT EXISTS recipe_tags (
+CREATE TABLE IF NOT EXISTS tags (
 	recipeTagID MEDIUMINT UNIQUE NOT NULL AUTO_INCREMENT,
 	tagName VARCHAR (32) NOT NULL,
 	PRIMARY KEY (recipeTagID)
