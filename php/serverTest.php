1efd32fd4e481b0f6a30b5a6b446c444bb0ddb97
SirBozwellox <jackgloyens@googlemail.com>
2018-05-03 00:05:33 +0100

    Fixed stuff

---
+++ php/serverTest.php
@@ -0,0 +1,15 @@
+<?php
+  require_once 'sessionCookieHandlerLib.php';
+  require_once 'queryLib.php';
+  require_once 'userCredentialsValidationLib.php';
+  startSession();
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Ceres server test</title>
+  </head>
+  <body>
+    <b>HELLO WORLD!</b
+  </body>
+</html>
